feb2fd22312782df2289215bb4aefd9945630b77
Arjun Mittal <arm197@pitt.edu>
2019-10-25 15:24:05 -0400

    This one has the pdf extension

--- tiger.R
+++ tiger.R
@@ -5,6 +5,6 @@ tigerTable <- sort(table(tigerData$activity), decreasing = TRUE)
 tigerTable
 data.frame(Frequency = tigerTable)
 data.frame(Frequency = addmargins(tigerTable))
-pdf("newTigerPlot")
+pdf("newTigerPlot.pdf")
 barplot(tigerTable, ylab = "Frequency", cex.names = 0.5, las = 2)
 dev.off()
